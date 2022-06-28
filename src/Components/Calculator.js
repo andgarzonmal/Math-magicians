3bc960b60431e5f8e70759a997945b66914a3a6b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-28 15:06:43 -0500

    create component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -14,25 +14,25 @@ class Calculator extends React.Component {
           <h1 className="solution">0</h1>
         </div>
         <div className="buttons-container">
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
-          <div className="buttons"><h1>content</h1></div>
+          <button type="button" className="buttons orange">AC</button>
+          <button type="button" className="buttons orange">+/-</button>
+          <button type="button" className="buttons orange">%</button>
+          <button type="button" className="buttons orange">÷</button>
+          <button type="button" className="buttons ">7</button>
+          <button type="button" className="buttons ">8</button>
+          <button type="button" className="buttons ">9</button>
+          <button type="button" className="buttons orange">x</button>
+          <button type="button" className="buttons ">4</button>
+          <button type="button" className="buttons ">5</button>
+          <button type="button" className="buttons ">6</button>
+          <button type="button" className="buttons orange">-</button>
+          <button type="button" className="buttons ">1</button>
+          <button type="button" className="buttons ">2</button>
+          <button type="button" className="buttons ">3</button>
+          <button type="button" className="buttons orange">+</button>
+          <button type="button" className="buttons cero">0</button>
+          <button type="button" className="buttons ">.</button>
+          <button type="button" className="buttons orange">=</button>
         </div>
       </section>
     );
