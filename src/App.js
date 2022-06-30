1266015deef43472573a2ab57a960184c42f48c8
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-30 12:13:00 -0500

    refactor class to function Es6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
 import React from 'react';
+// import { render } from '@testing-library/react';
 import Calculator from './Components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>My Calculator</h1>
-        <Calculator />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>My Calculator</h1>
+//         <Calculator />
+//       </div>
+//     );
+//   }
+// }
+
+//
+
+const App = () => (
+  <div className="App">
+    <h1>My Calculator</h1>
+    <Calculator />
+  </div>
+);
 
 export default App;
