c01acfff2f60e3776502bcd0a86e1564f5405860
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-04 23:21:25 -0500

    add quote Component

---
+++ src/Components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Quote.css';
+
+export default function Quote() {
+  return (
+    <div className="quote-container">
+      <p className="quote">
+        Mathematics is not about numbers, equations, computations,
+        or algorithms: it is about understanding.
+        {' '}
+        <i>-William Paul Thrurson</i>
+      </p>
+    </div>
+  );
+}
