98c38411589d50225147651e36a0146ba041c908
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-04 23:17:12 -0500

    Set up routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import { render } from '@testing-library/react';
 import Calculator from './Components/Calculator';
+import Home from './Components/Home';
+import Quote from './Components/Quote';
+import Navbar from './Components/Navbar';
 
 // class App extends React.Component {
 //   constructor(props) {
@@ -22,9 +26,32 @@ import Calculator from './Components/Calculator';
 //
 
 const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route
+        path="/Calculator"
+        element={(
+          <div className="App">
+            <Calculator />
+          </div>
+      )}
+      />
+      <Route
+        path="/"
+        element={(
+          <Home />
+      )}
+      />
+      <Route
+        path="/Quote"
+        element={(
+          <Quote />
+      )}
+      />
+    </Routes>
+  </BrowserRouter>
+
 );
 
 export default App;
