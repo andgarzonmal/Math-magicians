24cfdebe40accad63a78f304d096691db29f64aa
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-05 19:47:08 -0500

    reorganice project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import { render } from '@testing-library/react';
-import Calculator from './Components/Calculator';
-import Home from './Components/Home';
-import Quote from './Components/Quote';
-import Navbar from './Components/Navbar';
+import Calculator from './Components/Calculator/Calculator';
+import Home from './Components/Home/Home';
+import Quote from './Components/Quote/Quote';
+import Navbar from './Components/Navbar/Navbar';
 
 const App = () => (
   <BrowserRouter>
